d6f6f35d4e6fbca3f4e2f8943f614f35db1491c1
Viet Dang <thedangs7@gmail.com>
2018-12-03 10:04:00 +0200

    Add Slide effect for Time until session

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Slide from 'react-reveal/Slide';
 
 export default class TimeUntil extends Component {
   state = {
@@ -7,45 +8,48 @@ export default class TimeUntil extends Component {
 
   render() {
     return (
-      <div className="countdown_wrapper">
-        <div className="countdown_top">
-          Event starts in
+      <Slide left delay={1000}>
+        <div className="countdown_wrapper">
+          <div className="countdown_top">
+            Event starts in
         </div>
-        <div className="countdown_bottom">
-          <div className="countdown_item">
-            <div className="countdown_time">
+          <div className="countdown_bottom">
+            <div className="countdown_item">
+              <div className="countdown_time">
                 23
             </div>
-            <div className="countdown_tag">
-              days
+              <div className="countdown_tag">
+                days
             </div>
-          </div>
-          <div className="countdown_item">
-            <div className="countdown_time">
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">
                 2
             </div>
-            <div className="countdown_tag">
-              hrs
+              <div className="countdown_tag">
+                hrs
             </div>
-          </div>
-          <div className="countdown_item">
-            <div className="countdown_time">
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">
                 30
             </div>
-            <div className="countdown_tag">
-              min
+              <div className="countdown_tag">
+                min
             </div>
-          </div>
-          <div className="countdown_item">
-            <div className="countdown_time">
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">
                 40
             </div>
-            <div className="countdown_tag">
-              sec
+              <div className="countdown_tag">
+                sec
+            </div>
             </div>
           </div>
         </div>
-      </div>
+      </Slide>
+
     )
   }
 }
