0b8c22743b899d022137125cf5fd18e348959f56
Viet Dang <thedangs7@gmail.com>
2018-12-03 08:54:42 +0200

    Use Slide and add images to the slide

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 
-import Header from './components/header_footer/Header'
+import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{background: 'cornflowerblue', height:'2500px'}}>
         <Header/>
-       
+        <Featured />
       </div>
     );
   }
---
+++ src/components/featured/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Carrousel from './Carrousel';
+
+ const Featured = () => {
+  return (
+    <div style={{position: 'relative'}}>
+        <Carrousel />
+        <div className='artist_name'>
+          <div className='wrapper'>
+              Chau Nguyen
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured 
